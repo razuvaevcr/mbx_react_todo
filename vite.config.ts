c172437e2834de6475d1b388d0d423892ec153cf
razuvaevcr <razuvaj@gmail.com>
2024-06-07 21:59:39 +0300

    add .yml file

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mbx_react_todo/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
